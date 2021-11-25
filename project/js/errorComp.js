c8bf34c6922cfdd694b6c6c9c4ee5a37acd77b23
AntonPolskij <polskij.anton707@gmail.com>
2021-11-26 00:01:50 +0300

    server

--- project/js/errorComp.js
+++
@@ -1,8 +0,0 @@
-Vue.component ('error', {
-    data() {
-        return {
-            isError: false,
-        }
-    },
-    template: `<div class="error" v-show="isError"><h1>Ошибка загрузки данных</h1></div>`
-})
